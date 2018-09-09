bd93f770d825665bf4ca44cac3a87d87b46da64b
Patrick Vieira Feitosa <patrick.feitosa4@etec.sp.gov.br>
2018-09-09 15:10:22 -0300

    Refatorado rotas com o uso de Consign

--- src/mainApp.js
+++ src/mainApp.js
@@ -1,6 +1,4 @@
 const mainApp = require('./modules/config/MainServerConfig');
-const baseRoutes = require('./modules/routes/Routes__base-index');
-baseRoutes(mainApp);
 
 mainApp.listen(3000, () => {
     console.log('Servidor ON \nRodando na Porta 3000');
--- src/modules/config/MainServerConfig.js
+++ src/modules/config/MainServerConfig.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const consign = require('consign');
 const mainApp = express();
 
 //Setando a Engine de renderização e pasta padrão de views
 mainApp.set('view engine', 'ejs');
 mainApp.set('views', '../views');
+
+//Utilizando Consign para rotas
+consign().include('modules/routes').into(mainApp);
 
 module.exports = mainApp;
