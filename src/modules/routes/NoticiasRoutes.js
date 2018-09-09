718962009685e1d355b1bfe529accc2f946b0d3c
Patrick Vieira Feitosa <patrick.feitosa4@etec.sp.gov.br>
2018-09-09 13:48:46 -0300

    Implementado conexão com BD MySQL

--- src/modules/routes/NoticiasRoutes.js
+++ src/modules/routes/NoticiasRoutes.js
@@ -1,5 +1,16 @@
 module.exports = mainApp => {
     mainApp.get('/noticias', (request, response) => {
-        response.render('noticias/noticias');
+        const mysql = require('mysql');
+        const connectionDatabase = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '1234',
+            database: 'porta_noticias'
+        });
+
+        let queryString = 'select * from noticias'
+        connectionDatabase.query(queryString, (error, result) => {
+            response.render('noticias/noticias', {noticias: result});
+        });
     });
 }
