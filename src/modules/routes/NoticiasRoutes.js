45ba4ccfb14c84922bf81f181aab326ae4d8cd17
Patrick Vieira Feitosa <patrick.feitosa4@etec.sp.gov.br>
2018-09-09 14:55:14 -0300

    Modularizado configuração de Banco de Dados

--- src/modules/routes/NoticiasRoutes.js
+++ src/modules/routes/NoticiasRoutes.js
@@ -1,13 +1,7 @@
+const dbModule = require('../config/MainDatabaseConfig');
+const connectionDatabase = dbModule();
 module.exports = mainApp => {
     mainApp.get('/noticias', (request, response) => {
-        const mysql = require('mysql');
-        const connectionDatabase = mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: '1234',
-            database: 'porta_noticias'
-        });
-
         let queryString = 'select * from noticias'
         connectionDatabase.query(queryString, (error, result) => {
             response.render('noticias/noticias', {noticias: result});
