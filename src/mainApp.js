821c993159d6342544f80ffd34cce73ea70ed6dd
Patrick Vieira Feitosa <patrick.feitosa4@etec.sp.gov.br>
2018-09-09 02:30:29 -0300

    Incorporado EJS ao projeto e adicionado novas Views

--- src/mainApp.js
+++ src/mainApp.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const mainApp = express();
 
+//Setando a Engine de renderização e pasta padrão de views
+mainApp.set('view engine', 'ejs');
+mainApp.set('views', __dirname.replace('src', 'views'));
+
+console.log(__dirname);
+
 mainApp.get('/', (request, response) => {
     response.send(`<html><body>Portal de Notícias</body></html>`);
 });
 
 mainApp.get('/tecnologia', (request, response) => {
-    response.send(`<html><body>Notícias de Tecnologia</body></html>`);
+    response.render('secao/tecnologia');
 });
 
 mainApp.listen(3000, () => {
